0443359b26296af41bae4c8caf0a8746018ea0ca
ProdByArhaan <127174913+ProdByArhaan@users.noreply.github.com>
2025-03-13 20:26:53 -0600

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+// vite.config.js
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+export default defineConfig({
+  cacheDir: './node_modules/.vite_cache', // Add this line
+  plugins: [vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true
+      }
+    }
+  }
+})
